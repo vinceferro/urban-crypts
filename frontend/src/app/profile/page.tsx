1744b18266db40d409cbb6d21789770d20ca6e49
Vincenzo Ferrara <vinceferro92@gmail.com>
2023-11-19 16:51:52 +0100

    Adds rewards.

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -5,13 +5,21 @@ import { useRecoilValue } from 'recoil';
 import Collector from '@/abis/Collector.json';
 import Web3 from 'web3';
 import { useEffect, useState } from 'react';
-import { COLLECTOR_ADDRESS } from '@/constants';
+import {
+  COLLECTOR_ADDRESS,
+  CAN_TOKEN_ADDRESS,
+  GENERIC_PLASTIC_TOKEN_ADDRESS,
+  GLASS_TOKEN_ADDRESS,
+  PLASTIC_BOTTLE_TOKEN_ADDRESS,
+} from '@/constants';
 import { OnChainRecord, OnChainRecordStatus } from '../types';
-import { Badge, Table } from 'flowbite-react';
+import { Badge, Card, Table } from 'flowbite-react';
+import UrbanCryptToken from '@/abis/UrbanCryptToken.json';
 
 export default function Profile() {
   const walletAddress = useRecoilValue<string | undefined>(accountState);
   const [records, setRecords] = useState<OnChainRecord[]>([]);
+  const [tokenBalances, setTokenBalances] = useState<BigInt[]>([]);
 
   useEffect(() => {
     console.log(walletAddress);
@@ -30,12 +38,88 @@ export default function Profile() {
     getRecords().then((res) => {
       setRecords(res);
     });
+
+    async function getTokenBalance(address: string): Promise<BigInt> {
+      const token = new web3.eth.Contract(UrbanCryptToken.abi as any, address);
+
+      return await token.methods.balanceOf(walletAddress).call();
+    }
+
+    Promise.all([
+      getTokenBalance(CAN_TOKEN_ADDRESS),
+      getTokenBalance(GENERIC_PLASTIC_TOKEN_ADDRESS),
+      getTokenBalance(GLASS_TOKEN_ADDRESS),
+      getTokenBalance(PLASTIC_BOTTLE_TOKEN_ADDRESS),
+    ]).then((res) => {
+      setTokenBalances(res);
+    });
   }, [walletAddress]);
 
   console.log(records);
 
   return (
     <div className='xs:p-12 flex w-full flex-col items-center justify-center gap-4 p-24'>
+      <h1 className='text-2xl font-bold text-gray-900 dark:text-gray-100'>
+        Your Rewards
+      </h1>
+      <p className='text-gray-500 dark:text-gray-400'>
+        Those are your rewards on the blockchain.
+      </p>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
+        <Card>
+          <img src='/can-token.png' alt='can token' />
+          <div className='flex flex-col items-center justify-center'>
+            <p>
+              <span className='text-3xl font-bold text-gray-900 dark:text-gray-100'>
+                {Number(tokenBalances[0]) / 1000}&nbsp;
+              </span>
+              <span className='text-gray-500 dark:text-gray-400'>
+                Can Tokens
+              </span>
+            </p>
+          </div>
+        </Card>
+        <Card>
+          <img src='/generic-plastic-token.png' alt='generic plastic token' />
+          <div className='flex flex-col items-center justify-center'>
+            <p>
+              <span className='text-3xl font-bold text-gray-900 dark:text-gray-100'>
+                {Number(tokenBalances[1]) / 1000}&nbsp;
+              </span>
+              <span className='text-gray-500 dark:text-gray-400'>
+                Generic Plastic Tokens
+              </span>
+            </p>
+          </div>
+        </Card>
+        <Card>
+          <img src='/glass-token.png' alt='glass token' />
+          <div className='flex flex-col items-center justify-center'>
+            <p>
+              <span className='text-3xl font-bold text-gray-900 dark:text-gray-100'>
+                {Number(tokenBalances[2]) / 1000}&nbsp;
+              </span>
+              <span className='text-gray-500 dark:text-gray-400'>
+                Glass Tokens
+              </span>
+            </p>
+          </div>
+        </Card>
+        <Card>
+          <img src='/plastic-bottle-token.png' alt='plastic bottle token' />
+          <div className='flex flex-col items-center justify-center'>
+            <p>
+              <span className='text-3xl font-bold text-gray-900 dark:text-gray-100'>
+                {Number(tokenBalances[3]) / 1000}&nbsp;
+              </span>
+              <span className='text-gray-500 dark:text-gray-400'>
+                Plastic Bottle Tokens
+              </span>
+            </p>
+          </div>
+        </Card>
+      </div>
+
       <h1 className='text-2xl font-bold text-gray-900 dark:text-gray-100'>
         Records
       </h1>
